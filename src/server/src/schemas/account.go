5758f4141331b6769cc5608f1802cdbb45a7bca9
ivan mkk <mikhalchenko.i@gmail.com>
2022-08-13 13:20:57 +0200

    api: list items

--- src/server/src/schemas/account.go
+++ src/server/src/schemas/account.go
@@ -10,24 +10,6 @@ type AuthenticationRequest struct {
 	Password string `json:"password"`
 }
 
-type AddItem struct {
-	BrandName      string `json:"brand_name"`
-	GarmentId      string `json:"garment_id"`
-	SizeTypeId     string `json:"size_type_id"`
-	SizeTypeItemId string `json:"size_type_item_id"`
-}
-
-type GetItem struct {
-	Id             string `json:"id"`
-	BrandName      string `json:"brand_name"`
-	GarmentId      string `json:"garment_id"`
-	SizeTypeId     string `json:"size_type_id"`
-	SizeTypeItemId string `json:"size_type_item_id"`
-	ItemState      string `json:"item_state"`
-	DtimeCreated   string `json:"dtime_created"`
-	DtimeArchived  string `json:"dtime_archived"`
-}
-
 type Token struct {
 	TokenType   string `json:"token_type"`
 	AuthToken   string `json:"auth_token"`
