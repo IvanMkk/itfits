b200b37819cd803c403e2bcd3537a2f52dd0279e
Igor Vakhromeev <usfame@gmail.com>
2023-05-07 20:51:55 +0300

    sign out

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <nav>
+            <a href="#">Log In</a>
+            <a v-on:click="signout()" href="#">Sign Out</a>
+            <a href="#">Trash</a>
+            <a href="#">Wardrobe</a>
+        </nav>
+    </header>
+    <h1>Header</h1>
+</template>
+
+<script>
+    export default {
+        name: 'TheHeader',
+
+        methods: {
+            signout() {
+                localStorage.clear()
+                this.$router.push({name:'TheLogin'})
+            }
+        }
+    }
+</script>
--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,11 +1,17 @@
 <template>
+    <TheHeader />
     <main>
         <h1>Main page</h1>
     </main>
 </template>
 <script>
+import TheHeader from './TheHeader.vue'
 export default {
     name: 'TheHome',
+
+    components: {
+        TheHeader
+    },
 
     mounted() {
         let user = localStorage.getItem('user-info')
