dffe7cf38a549bc8d3134ec640f52fe782c63f02
Igor Vakhromeev <usfame@gmail.com>
2023-05-15 22:41:32 +0300

    folder front ready

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import '@/frontend/assets/css/style.css'
-//import '@/assets/css/style.css'
-import { createApp } from 'vue'
-import router from '@/frontend/routers'
-import App from '@/frontend/App.vue'
-
-/*import router from '/routers'*/
-
-createApp(App).use(router).mount('#app')
