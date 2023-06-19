08cd19beabc2124600818b76a6310e6adf5a60af
Igor Vakhromeev <usfame@gmail.com>
2023-06-19 21:45:13 +0300

    Revert "Revert "auth""
    This reverts commit c54ff9454f64ae7758d0e12e2cd9365aa1548e18.

--- src/front/src/main.js
+++ src/front/src/main.js
@@ -1,7 +1,7 @@
 import '@/assets/css/style.css'
 import { createApp } from 'vue'
 import router from '@/router/routers'
-import store from "@store/store"
+import store from "@/store/store"
 import App from './App.vue'
 
 const app = createApp(App)
